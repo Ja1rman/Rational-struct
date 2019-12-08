4fa1362326b92195093ca9be0afc706d73dc79b4
Roman Ovsyannikov <49319500+Ja1rman@users.noreply.github.com>
2019-12-08 22:41:07 +0300

    Update Rational_Struct.cpp

--- Rational_Struct.cpp
+++ Rational_Struct.cpp
@@ -1,151 +1,154 @@
-// Program was writing on c++ 17
+/*
+Реализуйте структуру LongInt, позволяющую использовать длинные натуральные числа, 
+а также пример использования. 
+Реализовать:
+конструкторы
+конструктор копий
+деструктор
+операторы >>,<< - ввод/вывод
+сложение, вычитание
+умножение (Карацуба)
+деление, взятие остатка (например, бисекцией)
+возведение в степень
+…
+Profit
+*/
 
 #include <iostream>
+#include <iterator>
 
 using namespace std;
 
-int gcd (int a, unsigned int b)
+struct LongInt
 {
-    if (b == 0)
-        return a;
-    return gcd (b, a % b);
-};
-
-struct Rational
-{
-    int m;
-    unsigned int n;
-    Rational(int a, unsigned int b){
-        m = a / gcd (a, b);
-        n = b / gcd (a, b);
-    }
-    Rational (Rational &X)
+    int n;
+    char* arr;
+    
+    LongInt(char* a, int b)
     {
-        m = X.m;
+        n = b;
+        char* arr = new char[n];
+        for (int i; i < n; i++)
+            arr[i] = a[i];
+    }
+    
+    LongInt(LongInt &X)
+    {
         n = X.n;
+        char* arr = new char[n];
+        for (int i; i < n; i++)
+            arr[i] = X.arr[i];
     }
-    Rational &operator*= (Rational b)
+    
+    ~LongInt()
     {
-        m *= b.m;
-        n *= b.n;
-        int g = gcd (m, n);
-        m /= g;
-        n /= g;
-        return *this;
+        delete [] arr;
     }
-    Rational &operator/= (Rational b)
+
+};
+
+LongInt operator+ (LongInt a, LongInt b)
+{
+    
+    if (a.n > b.n)
     {
-        m *= b.n;
-        n *= b.m;
-        int g = gcd (m, n);
-        m /= g;
-        n /= g;
-        return *this;
+        char* temp = new char[a.n + 1];
+        for (int i = 0; i <= a.n; i++)
+            temp[i] = 0;
+            
+        for (int i = a.n; i > 0; i--)
+        {
+            for (int j = b.n; j > 0; j--)
+            {
+                int p = (int)a.arr[i] + (int)b.arr[i];
+                if (p > 10)
+                {
+                    if (i != a.n)
+                        temp[i] = p % 10;
+                    else
+                        temp[i] = p % 10 + temp[i];
+                    temp[i + 1] = p / 10;
+                }
+            }
+        }
     }
-    Rational &operator+= (Rational b)
+    else
     {
-        n *= b.n;
-        m *= b.n;
-        b.m *= n;
-        m += b.m;
-        int g = gcd (m, n);
-        m /= g;
-        n /= g;
-        return *this;
+        char* temp = new char[b.n + 1];
+        for (int i = 0; i <= b.n; i++)
+            temp[i] = 0;
+            
+        for (int i = b.n; i > 0; i--)
+        {
+            for (int j = a.n; j > 0; j--)
+            {
+                int p = (int)a.arr[i] + (int)b.arr[i];
+                if (p > 10)
+                {
+                    if (i != b.n)
+                        temp[i] = p % 10;
+                    else
+                        temp[i] = p % 10 + temp[i];
+                    temp[i + 1] = p / 10;
+                }
+            }
+        }
     }
-    Rational &operator-= (Rational b)
-    {
-        n *= b.n;
-        m *= b.n;
-        b.m *= n;
-        m -= b.m;
-        int g = gcd (m, n);
-        m /= g;
-        n /= g;
-        return *this;
-    }
+    
+    
+    
 };
 
-Rational operator* (Rational a, Rational b)
+LongInt operator- (LongInt a, LongInt b)
 {
-    int p = a.m * b.m;
-    int q = a.n * b.n;
-    return Rational(p, q);
+    
 };
 
-Rational operator/ (Rational a, Rational b)
+LongInt operator* (LongInt a, LongInt b)
 {
-    int p = a.m * b.n;
-    int q = a.n * b.m;
-    return Rational(p, q);
+    
 };
 
-Rational operator+ (Rational a, Rational b)
+LongInt operator/ (LongInt a, LongInt b)
 {
-    int p = a.m * b.n + b.m * a.n;
-    int q = a.n * b.n;
-    return Rational(p, q);
+    
 };
 
-Rational operator- (Rational a, Rational b)
+LongInt operator% (LongInt a, LongInt b)
 {
-    int p = a.m * b.n - b.m * a.n;
-    int q = a.n * b.n;
-    return Rational(p, q);
+    
 };
 
-bool operator> (Rational a, Rational b)
+LongInt operator^ (LongInt a, LongInt b)
 {
-    return (a.m * b.n > b.m * a.n);
-};
-
-bool operator< (Rational a, Rational b)
-{
-    return (a.m * b.n < b.m * a.n);
-};
-
-bool operator<= (Rational a, Rational b)
-{
-    return !(a > b);
-};
-
-bool operator>= (Rational a, Rational b)
-{
-    return !(a < b);
-};
-
-bool operator== (Rational a, Rational b)
-{
-    return ((a.m == b.m) && (a.n == b.n));
-};
-
-ostream& operator<< (std::ostream& out, const Rational &a)
-{
-    return out << a.m << "/" << a.n;
-};
-
-istream& operator>> (std::istream& in, Rational &a)
-{
-    in >> a.m >> a.n;
-    int g = gcd (a.m, a.n);
-    a.m /= g;
-    a.n /= g;
-    return in;
+    
 };
 
 int main()
 {
-    int x, y;
-    cin >> x >> y;
-    cout << Rational(x, y) << endl;
-    cout << Rational(x, y) + Rational(y, x) << endl;
-    cout << Rational(x, y) - Rational(y, x) << endl;
-    cout << Rational(x, y) * Rational(y, x) << endl;
-    cout << Rational(x, y) / Rational(y, x) << endl;
-    cout << (Rational(x, y) > Rational(y, x)) << endl;
-    cout << (Rational(x, y) <= Rational(y, x)) << endl;
-    cout << (Rational(x, y) *= Rational(y, x)) << endl;
-    cout << (Rational(x, y) += Rational(y, x)) << endl;
-
+    cout << "Enter first number" << endl;
+    char* arr = new char;
+    cin.getline(arr, size_t(arr));
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    cout << "Enter second number" << endl;
+    char* arr2 = new char;
+    cin.getline(arr2, size_t(arr2));
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+    
+    cout << "Enter exponent" << endl;
+    int k;
+    cin >> k;
+    
+    cout << "+   " << (LongInt(arr, n) + LongInt(arr2, n2)) << endl;
+    /*cout << "-   " << LongInt(arr, n) - LongInt(arr2, n2) << endl;
+    cout << "*   " << LongInt(arr, n) * LongInt(arr2, n2) << endl;
+    cout << "/   " << LongInt(arr, n) / LongInt(arr2, n2) << endl;
+    cout << "%   " << LongInt(arr, n) % LongInt(arr2, n2) << endl;
+    cout << "pow   " << LongInt(arr) ^ k << endl;*/
+    
+    delete [] arr;
+    delete [] arr2;
+    
     return 0;
 }
